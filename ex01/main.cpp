b8e30d7444cbc1bb5a214c7fc7d006cfa0d9dbf0
javmarti <javier.martin.gallego@uva.es>
2023-08-23 10:38:21 +0200

    ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#include "Data.hpp"
+
+uintptr_t	serialize(Data* ptr);
+Data*	deserialize(uintptr_t raw);
+void	printData(Data* d);
+
+int	main(void)
+{
+	Data*		d = new Data();
+	uintptr_t	p;
+	Data*		result = NULL;
+
+	d->name = "El Nano";
+	d->age = 42;
+	printData(d);
+	p = serialize(d);
+	result = deserialize(p);
+	std::cout << "RESULT (uintptr_t = " << p << "): ";
+	printData(result);
+	delete d;
+	return 0;
+}
+
+uintptr_t	serialize(Data* ptr)
+{
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data*	deserialize(uintptr_t raw)
+{
+	return reinterpret_cast<Data*>(raw);
+}
+
+void	printData(Data* d)
+{
+	std::cout << "Data(" << d->name << ", " << d->age << ")" << std::endl;
+}
