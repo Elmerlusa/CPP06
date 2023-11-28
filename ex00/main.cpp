adda96c3562be581f30073466a48d4b199aaf1ec
javmarti <javier.martin.gallego@uva.es>
2023-11-28 15:08:21 +0100

    ex00: nueva clase abstracta ScalarConverter que sustituye a la clase Converter

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,27 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Converter.hpp"
-
-void	convertString(std::string str);
+#include "ScalarConverter.hpp"
 
 int	main(int argc, char* argv[])
 {
 	if (argc != 2)
 		std::cout << "Éste programa necesita un único número como parámetro" << std::endl;
 	else
-		convertString(argv[1]);
+		ScalarConverter::convert(argv[1]);
 	return 0;
-}
-
-void	convertString(const std::string str)
-{
-	try
-	{
-		Converter	c(str);
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
 }
