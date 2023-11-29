eff010a27f86a7aa22bb82a85ef1f142dd22e80c
javmarti <martingallegojavier@outlook.com>
2023-11-29 14:14:11 +0100

    ex00: main con try/catch

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: javmarti <javmarti@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 13:38:29 by javmarti          #+#    #+#             */
-/*   Updated: 2023/08/27 14:36:40 by javmarti         ###   ########.fr       */
+/*   Updated: 2023/11/29 14:13:53 by javmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ int	main(int argc, char* argv[])
 	if (argc != 2)
 		std::cout << "Éste programa necesita un único número como parámetro" << std::endl;
 	else
-		ScalarConverter::convert(argv[1]);
+	{
+		try
+		{
+			ScalarConverter::convert(argv[1]);
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << '\n';
+		}
+		
+	}
 	return 0;
 }
